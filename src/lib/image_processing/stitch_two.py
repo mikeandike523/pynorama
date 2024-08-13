0f5138689553a1d7a3a6c4a6f2b4bcbc6545ca80
Michael Sohnen <michaelsohnenacademic@gmail.com>
2024-08-13 18:19:30 -0400

    Made the gradient descent algorithm more intelligent byusing a dynamic step based on fitness. The process is still quite slow since need to create a more efficient MSE computation algorithm. That will be the next step.

--- src/lib/image_processing/stitch_two.py
+++ src/lib/image_processing/stitch_two.py
@@ -4,20 +4,29 @@ from typing import Protocol
 import cv2
 import numpy as np
 from termcolor import colored
-
 import warnings
-
-warnings.filterwarnings("error")
+from tqdm import tqdm
 
 
 from .RGBAImage import RGBAImage
 from .compute_overlapping_pixels import compute_overlapping_pixels
 from .apply_h_matrix_to_point import apply_h_matrix_to_point
-from .warp_without_cropping import warp_without_cropping
 
-
-GRADIENT_ASCENT_STEP = 2
-NUM_GRADIENT_ASCENT_ITERATIONS = 10
+# check N pixels in either direction to estimate the gradient
+# this is because its empirical and not known ahead of time
+# this is similar in concept to how normal vectors
+# are estimated in SDF computation and raymarching
+GRADIENT_ESTIMATE_RESOLUTION = 3
+# Usually determined empirically
+# Delta fitness is usually quite small due to logarithmic nature of fitness function
+# The absolute fitness value at the initial state is generally not relevant
+# An analogy may be an amplifier or gain for a sensitive instrument
+STEP_PIXELS_PER_DELTA_FITNESS = 3000
+# The maximum number of gradient ascent iterations
+NUM_GRADIENT_ASCENT_ITERATIONS = 5
+# Prevent travel of a corner if it is less (in magnitude) than this value
+# If all corners dont travel, stop gradient ascent
+TRAVEL_CUTOFF_PIXELS = 0.25
 
 
 class StitchParams(Protocol):
@@ -25,7 +34,7 @@ class StitchParams(Protocol):
     NUM_GOOD_MATCHES: int
     GOOD_MATCH_CUTOFF: float
     NUM_TREES: int
-    NUM_CHECKS: int
+    NUM_CHEßCKS: int
     RANSAC_REPROJECTION_THRESHOLD: float
 
 
@@ -152,30 +161,40 @@ def compute_mse_overlap(A: np.ndarray, B: np.ndarray, H: np.ndarray) -> float:
 
     num_overlap_coords = len(B_overlap_coords)
 
-    report_interval = int(0.1 * num_overlap_coords) + 1
+    with tqdm(range(num_overlap_coords), desc="Computing MSE", unit="pixels") as pbar:
 
-    for i, (x, y) in enumerate(list(B_overlap_coords)):
-        Ax, Ay = np.round(apply_h_matrix_to_point(np.array([x, y], float), H))
-        if Ax >= 0 and Ax < A.shape[1] and Ay >= 0 and Ay < A.shape[0]:
-            AR, AG, AB, AA = A[int(Ay), int(Ax), :4]
-            BR, BG, BB, BA = B[int(y), int(x), :4]
-            try:
-                if AA == 255 and BA == 255:
-                    value = (
-                        (
-                            (AR / 255 - BR / 255) ** 2
-                            + (AG / 255 - BG / 255) ** 2
-                            + (AB / 255 - BB / 255) ** 2
+        for x, y in list(B_overlap_coords):
+            Ax, Ay = np.round(apply_h_matrix_to_point(np.array([x, y], float), H))
+            if Ax >= 0 and Ax < A.shape[1] and Ay >= 0 and Ay < A.shape[0]:
+                AR, AG, AB, AA = A[int(Ay), int(Ax), :4]
+                BR, BG, BB, BA = B[int(y), int(x), :4]
+                try:
+                    warnings.filterwarnings("error")
+
+                    if AA == 255 and BA == 255:
+                        value = (
+                            (
+                                (AR / 255 - BR / 255) ** 2
+                                + (AG / 255 - BG / 255) ** 2
+                                + (AB / 255 - BB / 255) ** 2
+                            )
+                            * (255**2)
+                            / 3
                         )
-                        * (255**2)
-                        / 3
+                        result += value
+                    pbar.update(1)
+                except RuntimeWarning as e:
+                    print(
+                        "Arithmetic error or overflow occurred when calculating mse: {e}"
                     )
-                    result += value
-                if i % report_interval == 0:
-                    print(f"Processed {i}/{num_overlap_coords} overlapping pixels")
-            except RuntimeWarning as e:
-                print("Arithmetic error or overflow occurred when calculating mse: {e}")
-                raise e
+                    raise e
+                except Exception as e:
+                    print("An unknown error occured while calculating mse: {e}")
+                    raise e
+                finally:
+                    warnings.resetwarnings()
+            else:
+                pbar.update(1)
 
     return result / denom
 
@@ -186,7 +205,7 @@ def calculate_fitness(A, B, init_H, current_corners):
     assert A.shape == B.shape, "A and B must be of the same shape"
     H, W = A.shape[:2]
 
-    untransformed_corners = np.array([[0, 0], [W, 0], [W, H], [0, H]])
+    untransformed_corners = np.array([[0, 0], [W, 0], [W, H], [0, H]],float)
 
     init_mask_A, _ = compute_overlapping_pixels(A, B, init_H)
 
@@ -196,7 +215,7 @@ def calculate_fitness(A, B, init_H, current_corners):
         raise ZeroDivisionError("No overlapping pixels between A and B with initial H")
 
     current_H = cv2.getPerspectiveTransform(
-        np.array(current_corners).astype(np.float32),
+        current_corners.astype(np.float32),
         untransformed_corners.astype(np.float32),
     )
 
@@ -223,14 +242,19 @@ def filter_keypoints(keypoints, descriptors, mask):
     valid_keypoints = []
     valid_descriptors = []
 
+    num_skipped_keypoints = 0
+
     for keypoint, descriptor in zip(keypoints, descriptors):
         x, y = map(int, keypoint.pt)
         if not mask[y, x]:  # If the pixel is not transparent
             valid_keypoints.append(keypoint)
             valid_descriptors.append(descriptor)
         else:
-            # print(f"Skipping keypoint at ({x}, {y}) due to transparent region")
+            num_skipped_keypoints += 1
             pass
+
+    if num_skipped_keypoints > 0:
+        print(f"Skipped {num_skipped_keypoints} keypoints due to transparency")
 
     return valid_keypoints, np.array(valid_descriptors)
 
@@ -258,30 +282,23 @@ at {(100*tolerance_value):2.2f}% tolerance...
         try:
             iteration_params = copy.copy(STITCH_PARAMS)
             iteration_params.GOOD_MATCH_CUTOFF = tolerance_value
-            result = stitch_two(A.copy(), B.copy(), iteration_params)
-            fitness = calculate_fitness(
-                A,
-                B,
-                result,
-                np.array(
-                    [
-                        apply_h_matrix_to_point(point, result)
-                        for point in RGBAImage.from_pixels(A).corners()
-                    ],
-                    float,
-                ),
+            init_H = stitch_two(A.copy(), B.copy(), iteration_params)
+
+            print(
+                colored(
+                    f"Successfully found initial stitch estimate at tolerance {tolerance_value}",
+                    "green",
+                )
             )
-            print(colored("Refinement successful", "green"))
-            print(colored(f"fitness: {fitness}", "magenta"))
-            print(colored("Performing gradient descent", "yellow"))
-            init_H = result
-            current_corners = np.array(
+
+            init_estimate_corners = np.array(
                 [
-                    apply_h_matrix_to_point(point, result)
-                    for point in RGBAImage.from_pixels(A).corners()
+                    apply_h_matrix_to_point(point, init_H)
+                    for point in init_corners
                 ],
                 float,
             )
+            current_corners = init_estimate_corners.copy()
             for step in range(NUM_GRADIENT_ASCENT_ITERATIONS):
                 print(
                     colored(
@@ -289,7 +306,7 @@ at {(100*tolerance_value):2.2f}% tolerance...
                         "magenta",
                     )
                 )
-                parameters = current_corners.reshape((-1,))
+                parameters = current_corners.copy().ravel()
                 parameter_gradient = []
                 for parameter_index in range(len(parameters)):
                     print(
@@ -300,8 +317,12 @@ at {(100*tolerance_value):2.2f}% tolerance...
                     )
                     new_parameter_set_minus = list(parameters.copy())
                     new_parameter_set_plus = list(parameters.copy())
-                    new_parameter_set_minus[parameter_index] -= GRADIENT_ASCENT_STEP
-                    new_parameter_set_plus[parameter_index] += GRADIENT_ASCENT_STEP
+                    new_parameter_set_minus[
+                        parameter_index
+                    ] -= GRADIENT_ESTIMATE_RESOLUTION
+                    new_parameter_set_plus[
+                        parameter_index
+                    ] += GRADIENT_ESTIMATE_RESOLUTION
                     minus_fitness = calculate_fitness(
                         A,
                         B,
@@ -316,16 +337,15 @@ at {(100*tolerance_value):2.2f}% tolerance...
                     )
                     delta_fitness = plus_fitness - minus_fitness
                     parameter_gradient.append(
-                        delta_fitness / (2 * GRADIENT_ASCENT_STEP)
+                        delta_fitness / (2 * GRADIENT_ESTIMATE_RESOLUTION)
                     )
                 print(colored("Parameter Gradient", "blue"))
                 for item in parameter_gradient:
                     print(colored(str(item), "blue"))
-                coordinate_gradient = np.array(parameter_gradient, float).reshape(
+                coordinate_gradient = np.array(parameter_gradient, float).copy().reshape(
                     (-1, 2)
                 )
-                grad_A, grad_B, grad_C, grad_D = coordinate_gradient
-                # normalize gradients
+                grad_A, grad_B, grad_C, grad_D = coordinate_gradient.copy()
                 mag_A = np.linalg.norm(grad_A)
                 if abs(mag_A) < 1e-9:
                     raise ValueError("Gradient magnitude of A is too small")
@@ -341,28 +361,81 @@ at {(100*tolerance_value):2.2f}% tolerance...
                 mag_D = np.linalg.norm(grad_D)
                 if abs(mag_D) < 1e-9:
                     raise ValueError("Gradient magnitude of D is too small")
-                delta_A = grad_A * GRADIENT_ASCENT_STEP
-                delta_B = grad_B * GRADIENT_ASCENT_STEP
-                delta_C = grad_C * GRADIENT_ASCENT_STEP
-                delta_D = grad_D * GRADIENT_ASCENT_STEP
+                grad_D /= mag_D
+                added_fitness_A = np.linalg.norm(coordinate_gradient[0])
+                added_fitness_B = np.linalg.norm(coordinate_gradient[1])
+                added_fitness_C = np.linalg.norm(coordinate_gradient[2])
+                added_fitness_D = np.linalg.norm(coordinate_gradient[3])
+                travel_A = added_fitness_A * STEP_PIXELS_PER_DELTA_FITNESS
+                travel_B = added_fitness_B * STEP_PIXELS_PER_DELTA_FITNESS
+                travel_C = added_fitness_C * STEP_PIXELS_PER_DELTA_FITNESS
+                travel_D = added_fitness_D * STEP_PIXELS_PER_DELTA_FITNESS
+                print(colored("corner gradients (normalized/direction-only) this iteration:", "green"))
+                print("x\ty")
+                for grad_x,grad_y in [grad_A, grad_B, grad_C, grad_D]:
+                    print(f"{grad_x:.2f}" + f"\t{grad_y:.2f}")
+                print(colored("Corner travel magnitudes this iteration:", "green"))
+                for travel in [travel_A, travel_B, travel_C, travel_D]:
+                    print(f"{travel}"
+                          +
+                          f"{"(cutoff)" if abs(travel) < TRAVEL_CUTOFF_PIXELS else ""}")
+                if (abs(travel_A) < TRAVEL_CUTOFF_PIXELS
+                    and abs(travel_B) < TRAVEL_CUTOFF_PIXELS
+                    and abs(travel_C) < TRAVEL_CUTOFF_PIXELS
+                        and abs(travel_D) < TRAVEL_CUTOFF_PIXELS):
+                    print(colored(f"""
+Gradient ascent early stop condition reached
+No corner traveled >= {TRAVEL_CUTOFF_PIXELS} pixels
+""","yellow"))
+                    break
+
+                travel_A = travel_A if abs(travel_A) >= TRAVEL_CUTOFF_PIXELS else 0
+                travel_B = travel_B if abs(travel_B) >= TRAVEL_CUTOFF_PIXELS else 0
+                travel_C = travel_C if abs(travel_C) >= TRAVEL_CUTOFF_PIXELS else 0
+                travel_A = travel_D if abs(travel_D) >= TRAVEL_CUTOFF_PIXELS else 0
+
+
+
+                delta_A = grad_A * travel_A
+                delta_B = grad_B * travel_B
+                delta_C = grad_C * travel_C
+                delta_D = grad_D * travel_D
+
                 current_corners = np.array(
                     [
-                        (np.array(corner, float) + np.array(delta, float))
+                        corner+delta
                         for (corner, delta) in zip(
-                            current_corners, [delta_A, delta_B, delta_C, delta_D]
+                            list(current_corners), [delta_A, delta_B, delta_C, delta_D]
                         )
                     ],
                     float,
                 )
-                result = cv2.getPerspectiveTransform(
-                    (init_corners).astype(np.float32),
-                    (current_corners).astype(np.float32),
-                )
-            return result
+
+
+
+
+                print(colored(f"Corner deviation after iteration {step}:", "green"))
+                print("x\ty")
+                for init_estimate_corner, current_corner in zip(init_estimate_corners, current_corners):
+                    delta_x, delta_y = current_corner - init_estimate_corner
+                    print(f"{delta_x:.2f}\t{delta_y:.2f}")
+
+            return cv2.getPerspectiveTransform(
+                (init_corners).astype(np.float32),
+                (current_corners).astype(np.float32),
+            )
         except InsufficientMatchesError as e:
             print(colored(str(e), "red"))
         tolerance_value += ITERATION_TEST_STEP
 
     raise ValueError(
-        "Could not find enough matches up to 50% tolerance. Stitch cannot converge."
+        """
+        Could not find initial stitch estimate (insufficient SIFT matches)
+        or could not complete gradient ascent
+        (e.g. init fitness too close to zero) before reaching 50% tolerance
+        Generally, tolerances more than 50% are fully irrelevant
+        Consider downsampling your input images
+        Recommended settings are downsampling by a factor of 2
+        with bilinear interpolation
+        """
     )
